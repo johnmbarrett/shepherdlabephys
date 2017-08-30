605fe7b748cd7f80c66f2bf45684c57844c430b1
John Barrett <barrett.m.john@googlemail.com>
2017-08-30 16:58:25 -0500

    Implement VideoMapRecording.fromMATFile

--- analysis/+mapAnalysis/@VideoMapRecording/VideoMapRecording.m
+++ analysis/+mapAnalysis/@VideoMapRecording/VideoMapRecording.m
@@ -60,12 +60,75 @@ classdef VideoMapRecording < mapAnalysis.Recording
         hs = plotSkew(self,bregmaCoordsPx); % TODO : better name
         
         hs = plotTrajectories(self,l,varargin)
+        
+        function name = getRecordingName(~)
+            name = 'FUCK YOU';
+        end
     end
     
     methods(Static=true)
-        % TODO : pull implementation from motor mapping package
-        function mmr = fromMATFile(matFile) % TODO : a superclass that knows about its subclasses?  is that allowed?  maybe a factory class would be better
-            mmr = mm.MATFileMotorMappingResult(matFile);
+        function pattern = standardPattern(rows,cols)
+            n = rows*cols;
+            
+            pattern = flipud(reshape(1:n,rows,cols));
+        end
+        
+        function recording = fromMATFile(matFile,rows,cols) % TODO : a superclass that knows about its subclasses?  is that allowed?  maybe a factory class would be better
+            recording = mapAnalysis.VideoMapRecording;
+            
+            load(matFile);
+            
+            assert(logical(exist('map','var')),'ShepherdLab:mapAnalysis:VideoMapRecording:fromMATFile:TotalMovementNotFound','File %s does not contain a motor map\n',matFile); % TODO : generate if missing
+            assert(logical(exist('motionTubes','var')),'ShepherdLab:mapAnalysis:VideoMapRecording:fromMATFile:MotionTubesNotFound','File %s does not contain any motion tubes\n',matFile);
+            assert(logical(exist('pathLengths','var')),'ShepherdLab:mapAnalysis:VideoMapRecording:fromMATFile:PathLengthsNotFound','File %s does not contain any path lengths\n',matFile);
+            assert(logical(exist('roiPositions','var')),'ShepherdLab:mapAnalysis:VideoMapRecording:fromMATFile:ROIsNotFound','File %s does not contain any ROI coordinates\n',matFile);
+            assert(logical(exist('trajectories','var')),'ShepherdLab:mapAnalysis:VideoMapRecording:fromMATFile:TrajectoriesNotFound','File %s does not contain any trajectories\n',matFile);
+            
+            if nargin < 2
+                n = size(map,1);
+                m = sqrt(n);
+                
+                if round(m) == m
+                    rows = m;
+                    cols = m;
+                elseif isprime(n)
+                    rows = n;
+                    cols = 1;
+                else
+                    % assume the squarest map we can, with more rows than
+                    % columns, because the mouse brain is long
+                    factors = [1; unique(cumprod(perms(factor(12)),2))];
+                    
+                    k = numel(factors);
+                    assert(mod(k,2) == 0); % it's not square, so it should have an even number of factors
+                    
+                    rows = factors(k/2+1);
+                    cols = factors(k/2);
+                end
+            elseif nargin < 3
+                cols = rows;
+            end
+            
+            pattern = mapAnalysis.VideoMapRecording.standardPattern(rows,cols);
+            
+            recording.TotalMovement = mapAnalysis.Map(map,pattern);
+            recording.MotionTubes = mapAnalysis.Map(motionTubes,pattern); % TODO : make the cell maps arrays?
+            recording.PathLengths = mapAnalysis.Map(pathLengths,pattern);
+            recording.Trajectories = mapAnalysis.Map(trajectories,pattern);
+            
+            if exist('files','var')
+                recording.Files_ = files;
+            else
+                warning('ShepherdLab:mapAnalysis:VideoMapRecording:fromMATFile:FilesNotFound','Full paths were not saved in motor mapping result file %s, assuming files have standard naming and are located in the same folder as the result file.\n',matFile);
+                
+                [~,attributes] = fileattrib(matFile);
+            
+                path = fileparts(attributes.Name);
+                
+                load(matFile,'map');
+                
+                recording.Files_ = arrayfun(@(ii) sprintf('%s\\VT%d.mat',path,ii-1),1:size(map,1),'UniformOutput',false);
+            end
         end
         
         function mmr = fromVideoFiles(videoFiles,varargin) % TODO : other parameters
